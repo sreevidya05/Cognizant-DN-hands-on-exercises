7f9e98b6a6a3a3bdebe90ceb996b8426332e4bb5
Sree Vidhya <gobburisreevidya@gmail.com>
2026-07-02 19:18:21 +0530

    Completed Week 1 - Exercise 1: Singleton Pattern

--- Week-1-Engineering-Concepts/Exercise-1-Singleton-Pattern/src/Main.java
+++ Week-1-Engineering-Concepts/Exercise-1-Singleton-Pattern/src/Main.java
@@ -1,1 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
 
+        SingletonPattern obj1 = SingletonPattern.getInstance();
+        SingletonPattern obj2 = SingletonPattern.getInstance();
+
+        obj1.display();
+
+        if (obj1 == obj2) {
+            System.out.println("Both objects are the same instance.");
+        }
+    }
+}
