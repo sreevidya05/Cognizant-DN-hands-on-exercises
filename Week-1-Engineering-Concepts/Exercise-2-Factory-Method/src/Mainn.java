9e8c56218daa8627e34c22412e821d790d75779b
Sreevidya Gobburi <gobburisreevidya@gmail.com>
2026-07-27 22:27:36 +0530

    Delete Week-1-Engineering-Concepts directory

--- Week-1-Engineering-Concepts/Exercise-2-Factory-Method/src/Mainn.java
+++
@@ -1,8 +0,0 @@
-public class Mainn {
-    public static void main(String[] args) {
-        DocumentFactory factory = new WordFactory() ;
-        Document document = factory.createDocument();
-        document.open();
-
-    }
-}
